34cc03fd316bf85a9e2bca99f32d6f61aad2f5fd
Judin <dafajudin@student.telkomuniversity.ac.id>
2022-12-21 19:00:06 +0700

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <header>
+        <ul>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="blog">Blog</Link></li>
+          <li><Link href="users">Users</Link></li>
+        </ul>
+      </header>
+    </>
+  )
+}
---
+++ pages/users/index.tsx
@@ -0,0 +1,1 @@
+import default function Users() {
