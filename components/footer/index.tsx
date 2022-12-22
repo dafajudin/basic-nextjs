c6c917b76e91fffb692bcc75818ce23da101b20f
Judin <dafajudin@student.telkomuniversity.ac.id>
2022-12-22 09:02:29 +0700

    upload index.tsx

---
+++ components/footer/index.tsx
@@ -0,0 +1,9 @@
+import styles from './footer.module.css'
+
+export default function Header() {
+    return (
+        <div>
+            <p className={styles.title}>Made with love @judin</p>
+        </div>
+    )
+}
