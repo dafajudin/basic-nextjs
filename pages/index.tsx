93722f3e522f47c3e1ec7d25b2882205b7e947ba
Judin <dafajudin@student.telkomuniversity.ac.id>
2022-12-22 10:43:06 +0700

    update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
-import Header from '../components/headers'
-import Footer from '../components/footer'
 import styles from '../styles/Home.module.css'
+import Layout from '../components/layout';
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <h1 className={styles['title-homepage']}>Welcome Judin</h1>
-      <Footer />
-    </>
+      <Layout>
+        <h1 className={styles['title-homepage']}>Welcome Judin</h1>
+      </Layout>
   );
 }
